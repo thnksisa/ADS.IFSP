3d06a5f56ad682ac10ed1eb9baf7fbcc782097e8
Isabella <146090324+thnksisa@users.noreply.github.com>
2025-03-18 19:41:08 -0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,20 @@
+'''Faça um programa que leia o ano de nascimento de uma pessoa e imprima se ela é
+maior ou menor de idade. Declare o ano atual e o limite de maioridade como
+constantes simbólicas.'''
+
+# Definindo constantes simbólicas:
+
+ANO_ATUAL = 2025
+LIMITE_MAIOR = 18
+
+# INÍCIO
+
+print("Insira seu ano de nascimento:")
+ano = int(input("➔ "))
+
+idade = ANO_ATUAL - ano
+
+if idade >= LIMITE_MAIOR:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
