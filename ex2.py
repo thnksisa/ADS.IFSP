8858cdaf578708c73c29803258c493b6ca529887
Ykkiirz <128730671+Ykkiirz@users.noreply.github.com>
2025-03-27 11:40:44 -0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,3 +1,12 @@
+'''Em um determinado país, deve declarar imposto de renda todo cidadão com renda
+anual superior à $ 23.750,00. A renda anual é a renda mensal multiplicada por 13 (12
+meses mais a o 13o salário). A alíquota para quem paga é de 20%. Faça um programa
+que leia do teclado a renda mensal do usuário e imprima se ele está isento ou se ele
+deve fazer a declaração de renda e qual o imposto devido. Declare como constantes
+simbólicas o limite para imposto: 23750; o fator de multiplicação: 13; e a alíquota:
+20%.'''
+
+
 renda_maxima = 23750
 renda_mensal = float(input("Digite sua renda mensal R$: "))
 renda_anual =  renda_mensal * 13
