add23b442681bf5d8db0e104d20d77031a5ebae0
Isa Mou <isabellemourasoares@gmail.com>
2025-03-28 21:11:09 -0300

    ex13.py

--- ex13.py
+++ ex13.py
@@ -4,8 +4,11 @@ três lidos. O que acontece se o seu programa tiver lido dois ou mais números i
 
 a, b, c = input("Insira 3 valores separados com espaço: ").split( )
 Lista_num = [a, b, c]
+'''.split() faz com que quando os números sao inseridos como input,
+eles sao separados com um espaço em vez de vígula.'''
 
 menor = min(Lista_num)
 print(menor)
+''' min seleciona o menor numero em um lista'''
 
 #ao inserir dois ou mais números iguais, o programa continua monstrando apenas um número como saída
