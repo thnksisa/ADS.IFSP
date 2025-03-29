70345ba3a666c7da061a4d0687f3a850cabe1956
Isa Mou <isabellemourasoares@gmail.com>
2025-03-28 21:12:34 -0300

    ex15.py

--- ex15.py
+++ ex15.py
@@ -4,8 +4,11 @@ anterior? '''
 
 a, b, c = input("Insira 3 valores separados com espaço: ").split( )
 Lista_num = [a, b, c]
+'''.split() faz com que quando os números sao inseridos como input,
+eles sao separados com um espaço em vez de vígula.'''
 
 Lista_num.sort(key=int)
 print(Lista_num)
+'''.sort arranja uma lista em ordem cresceste e o key=int faz com que todos os numeros na lista se tornem inteiros'''
 
 #Ao inserir um ou mais valores identicos, eles sao repetidos durante a saida final
