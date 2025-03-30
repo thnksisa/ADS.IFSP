0597192d98d94e53cb77ec684df30683026d18dd
victor-moreira1 <victormoreira247@gmail.com>
2025-03-30 12:46:50 -0300

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -11,18 +11,19 @@ dos comprimentos dos outros dois lados.
 '''
 
 #Lados:
-a = int(input("Digite o tamanho do lado 'a': "))
-b = int(input("Digite o tamanho do lado 'b': "))
-c = int(input("Digite o tamanho do lado 'c': "))
+a = float(input("Digite o tamanho do lado 'a': "))
+b = float(input("Digite o tamanho do lado 'b': "))
+c = float(input("Digite o tamanho do lado 'c': "))
 
 # Função Modular
 def mod(x):
     if x < 0:
+        # se x for negativo, inverte para positivo:
         return -x
     else:
         return x
 
-# Desigualdade Triangular
+# Desigualdade Triangular (verificação de existência de triângulo)
 if a + b <= c:
     print("Valores inválidos para formar um triângulo.")
 elif c <= mod(a - b):
@@ -31,9 +32,12 @@ elif c <= mod(a - b):
 # Verificação do tipo de Triângulo:
 else:
     if a == b == c:
+        # se todos os lados forem iguais:
         print("Este é um triângulo equilátero.")
     elif a == b or a == c or b == c:
+        # se algum lado for igual a outro:
         print("Este é um triângulo isósceles.")
     else:
+        # se todos os lados forem diferentes:
         print("Este é um triângulo escaleno.")
 
