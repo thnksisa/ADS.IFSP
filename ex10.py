cfdd689d1cfe9c142bc9f6dfe60a81cfbd0783ca
Ykkiirz <128730671+Ykkiirz@users.noreply.github.com>
2025-03-30 20:42:26 -0300

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,20 @@
+''''Faça um programa que lê um caracter do teclado e imprima se o caracter é uma letra.
+Se for, deve imprimir se a letra em questão é maiúscula ou minúscula. Dica: use os
+códigos ASCII das letras para resolver este problema.'''
+
+
+letra = (input("Escreva uma letra: "))
+
+
+if letra.isupper():
+        print(f"A letra {letra} é maiuscula")
+elif letra.islower():
+        print(f"A letra {letra} é minuscula")
+else: 
+        letra == int
+        print("É um número não uma letra: ")
+
+#Comando letra detecta qual é variavel e escrita e replica nas condicionais, para imprimir se é maiuscula ou minuscula, porém se for número, exibira a mensagem final
+
+
+
